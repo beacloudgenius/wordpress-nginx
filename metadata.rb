17b7c8c7486870561aff6e50ea94b4a9c497ee59
Nilesh Londhe <nilesh@londhe.com>
2014-08-01 21:50:12 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,5 @@ description      'Installs/Configures Nginx sites for WordPress with PHP-FPM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.0'
 
-depends          'nginx', '<= 1.7.0'
+depends          'nginx'
 depends          'php-fpm'
